6528e126a7325003b58c2835d4c09632e257e2d9
gokul raman <sgokulraman5@gmail.com>
2025-12-23 21:09:16 +0530

    Todo List

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css'
+// import Slider from './Slider/Slider';
+// import Navbar from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Example from './Example';
+import 'bootstrap/dist/js/bootstrap.min.js'
+// import Card from './Card/Card';
+// import Count from './Count';
+// import CounterClass from './CounterClass';
+// import Newnav from './DifferentNav/Newnav';
+// import Newnav1 from './DifferentNav/Newnav1';
+
+function App() {
+
+  return (
+    <div>
+      {/* <Newnav/>
+      <Newnav1/> */}
+      {/* <Navbar/> */}
+      {/* <Slider/> */}
+      <Example/>
+      {/* <CounterClass/> */}
+      {/* <Card/> */}
+      {/* <Count/> */}
+    </div>
+  )
+}
+
+export default App
